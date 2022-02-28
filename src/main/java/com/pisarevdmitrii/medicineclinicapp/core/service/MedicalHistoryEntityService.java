b9bb0767d3146076813c17e8077fd94b581ced02
Pisarev <d.pisarev.03@mail.ru>
2022-02-28 18:48:51 +0300

    fixed bugs in using annotations

--- src/main/java/com/pisarevdmitrii/medicineclinicapp/core/service/MedicalHistoryEntityService.java
+++ src/main/java/com/pisarevdmitrii/medicineclinicapp/core/service/MedicalHistoryEntityService.java
@@ -2,7 +2,5 @@ package com.pisarevdmitrii.medicineclinicapp.core.service;
 
 import com.pisarevdmitrii.medicineclinicapp.core.model.entity.MedicalHistoryEntity;
 
-import java.util.List;
-
 public interface MedicalHistoryEntityService extends CrudService<MedicalHistoryEntity> {
 }
--- src/main/java/com/pisarevdmitrii/medicineclinicapp/core/service/PatientEntityService.java
+++ src/main/java/com/pisarevdmitrii/medicineclinicapp/core/service/PatientEntityService.java
@@ -2,8 +2,5 @@ package com.pisarevdmitrii.medicineclinicapp.core.service;
 
 import com.pisarevdmitrii.medicineclinicapp.core.model.entity.PatientEntity;
 
-import java.util.List;
-import java.util.UUID;
-
 public interface PatientEntityService extends CrudService<PatientEntity> {
 }
