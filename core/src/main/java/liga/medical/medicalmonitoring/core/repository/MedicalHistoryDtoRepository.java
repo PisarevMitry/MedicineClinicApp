4409abe8225b2c6a031e1ff5265e1eb355022022
Pisarev <d.pisarev.03@mail.ru>
2022-03-10 14:21:59 +0300

    некоторые изменения

--- core/src/main/java/liga/medical/medicalmonitoring/core/repository/MedicalHistoryDtoRepository.java
+++ core/src/main/java/liga/medical/medicalmonitoring/core/repository/MedicalHistoryDtoRepository.java
@@ -2,7 +2,9 @@ package liga.medical.medicalmonitoring.core.repository;
 
 import liga.medical.medicalmonitoring.dto.MedicalHistoryDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MedicalHistoryDtoRepository extends JpaRepository<MedicalHistoryDto, Long> {
 
 }
--- core/src/main/java/liga/medical/medicalmonitoring/core/repository/PatientDtoRepository.java
+++ core/src/main/java/liga/medical/medicalmonitoring/core/repository/PatientDtoRepository.java
@@ -2,8 +2,9 @@ package liga.medical.medicalmonitoring.core.repository;
 
 import liga.medical.medicalmonitoring.dto.PatientDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-//@Repository
+@Repository
 public interface PatientDtoRepository extends JpaRepository<PatientDto, Long> {
 
 }
